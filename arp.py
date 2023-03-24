dbe164440e51ad26e41e243787218b8b0ef551f3
kaine119 <9492636+kaine119@users.noreply.github.com>
2023-03-24 21:29:47 +0800

    Match on the last 3 bytes of the MAC address
    This lets you use the RMTT- SSID on the head unit instead of the full MAC
    address.

--- arp.py
+++ arp.py
@@ -14,5 +14,5 @@ def arp_scan():
     clients = {}
 
     for sent, received in result:
-        clients[received.hwsrc] = received.psrc
+        clients[received.hwsrc[-8:].lower()] = received.psrc
     return clients
